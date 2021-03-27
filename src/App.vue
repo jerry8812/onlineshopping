b6edcf513ebedb1ceb4672c25d7ca5791e687987
Jerry <jerry8812@hotmail.com>
2021-03-27 23:42:57 +1300

    install axios and test getting data from backend server

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    
+    {{test}}
   </div>
 </template>
 
 <script>
 
+import {request} from './network/request'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      test: ''
+    }
+  },
+  created() {
+    request({
+      url: '/test/findAll'
+    }).then(res => {
+      this.test = res.data
+    })
+  }
 }
 </script>
 
